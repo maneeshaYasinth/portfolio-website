eb504d4c8909c851993697a0ce0de24a77cf9903
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2026-02-22 03:31:34 +0530

    modified hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,25 +1,28 @@
 export default function Hero() {
   return (
-    <section className="min-h-screen bg-white flex items-center justify-center">
+    <section className="min-h-screen bg-white flex items-center justify-center overflow-x-hidden pt-16 md:pt-32">
       <div className="w-full max-w-7xl mx-auto px-4 md:px-8 lg:px-4 py-10 text-start relative">
-        <p className="text-orange-600 tracking-[0.4em] uppercase text-sm font-bold pb-1 mb-1">
+        <p className="text-orange-600 tracking-[0.25em] sm:tracking-[0.4em] uppercase text-xs sm:text-sm font-bold pb-1 mb-1">
           Maneesha Gunarathna
         </p>
 
-        <h1 className="mt-2 uppercase leading-[0.9] font-black tracking-[-0.02em] text-[clamp(2.5rem,7vw,7rem)] font-['Krona_One']">
-          <span className="block">SCALABLE CLOUD</span>
-          <span className="block">SYSTEMS.</span>
-
-          <span
-            className="block mt-6 text-transparent"
-            style={{ WebkitTextStroke: "2px #f97316" }}
-          >
-            FULL-STACK EXECUTION.
+        <h1 className="mt-2 uppercase leading-[0.9] sm:leading-[0.85] font-black tracking-[0.12rem] sm:tracking-[0.3rem] text-[clamp(2rem,9vw,8rem)] font-['Krona_One']">
+          <span className="block sm:whitespace-nowrap">
+            SCALABLE
+          </span>
+          <span className="block sm:whitespace-nowrap">
+            CLOUD 
+          </span>
+          <span className="block sm:whitespace-nowrap">
+           SYSTEMS.
+          </span>
+        <span className="block mt-6 text-transparent" style={{ WebkitTextStroke: "2px #f97316" }} > 
+          FULL-STACK EXECUTION. 
           </span>
         </h1>
 
         <div className="mt-12 flex flex-col md:flex-row items-start gap-8 md:gap-10">
-          <p className="text-black max-w-[32ch] text-[clamp(1.5rem,1vw,2.8rem)] leading-[1.2] text-start font-[Pixelify_Sans]">
+          <p className="text-black max-w-[32ch] text-[clamp(1rem,2.8vw,1.5rem)] leading-[1.25] text-start font-[Pixelify_Sans]">
             I architect resilient cloud infrastructures and high-performance
             digital experiences. Crafting the backbone of the modern web.
           </p>
