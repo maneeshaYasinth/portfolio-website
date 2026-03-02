4e6e57c26f71437b63b75f98564ab78e6edf70a4
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2026-03-03 00:36:34 +0530

    added new achivement

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -1,5 +1,6 @@
 import rextro1st from "../assets/rextro1st.webp";
 import hackx from "../assets/hackx.webp";
+import pitch60 from "../assets/pitch60.jpeg";
 
 
 
@@ -23,5 +24,15 @@ export const achievements = [
       "Served as System Developer for Hydrotech - Smart H2O, delivering the core architecture and technical implementation that secured 1st Runner-Up at Pitch Arena 2025. Designed and executed key system components, translating concept into a functional, impact-driven solution through structured engineering and team collaboration.",
     image:rextro1st,
     link: "https://web.facebook.com/share/p/18AHRDxFtt/",
+  },
+    {
+    id: 3,
+    year: "2026",
+    title: "University Innovation Challenge - Pitch 60 2026",
+    result: "Most Innovative Pitch Award",
+    description:
+      "Served as System Developer for Hydrotech - Smart H2O, leading the end-to-end system architecture and technical execution that earned the Most Innovative Pitch Award at Pitch60 2026. Engineered the core solution framework, integrated critical components, and translated strategic vision into a scalable, real-world prototype. Delivered measurable innovation through structured design, technical precision, and high-performance team collaboration.",
+    image:pitch60,
+    link: "#",
   },
 ];
